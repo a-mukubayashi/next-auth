6d55367dffd32e817223ab5a42a495d98ff42ba3
a-mukubayashi <akawasumi92@gmail.com>
2022-08-26 21:02:25 +0900

    docs: typo

--- cypress/e2e/header.cy.ts
+++ cypress/e2e/header.cy.ts
@@ -3,13 +3,13 @@ describe("Header", () => {
     // Start from the index page
     cy.visit("http://localhost:3000/");
 
-    // Find a link with an href attribute containing "about" and click it
+    // Find a link with an href attribute containing "signin" and click it
     cy.get('a[href*="/api/auth/signin"]').click();
 
-    // The new url should include "/about"
+    // The new url should include "signin"
     cy.url().should("include", "/api/auth/signin");
 
-    // The new page should contain an h1 with "About page"
+    // The new page should contain an h1 with "Sign in with GitHub"
     cy.get("button.button").contains("Sign in with GitHub");
   });
 });
