7f93c2065a530e5d1b81edd34febba3a67e58d89
a-mukubayashi <akawasumi92@gmail.com>
2022-08-27 21:25:46 +0900

    test: github login testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "cypress";
+const { GitHubSocialLogin } = require("cypress-social-logins").plugins;
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
+    setupNodeEvents(on) {
+      on("task", {
+        GitHubSocialLogin,
+      });
+    },
   },
+  chromeWebSecurity: false,
 });
